197e82f781ccb91a47684f8fda297395da6407d7
Zach Frohardt <zfrohardt@gmail.com>
2021-05-17 12:40:23 -0700

    commit tree 09da9ab7389ca5ee62082a7b60b6da320012fa6f

--- prime.rb
+++ prime.rb
@@ -1,1 +1,16 @@
 # Add  code here!
+def prime?(n)
+    if n <= 3
+        return n > 1
+    elsif n % 2 == 0 || n % 3 == 0
+        return false
+    end
+    i = 5
+    while i * i <= n
+        if n % i == 0 || n % (i + 2) == 0
+            return false
+        end
+        i += 6
+    end
+    true
+end
